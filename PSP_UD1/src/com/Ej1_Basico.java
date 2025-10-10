615fd1de459bb309d5caea80df4625b5d69c0824
Jose Antonio Serrano <serrano.santos.jose.antonio@iescamas.es>
2025-10-10 19:27:52 +0200

    Actualizacion de ejercicios

---
+++ PSP_UD1/src/com/Ej1_Basico.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Ej1_Basico {
+
+	public static void main(String[] args) throws Exception {
+	    Process p = new ProcessBuilder(Utils.sh("echo hola")).start();
+	    int rc = p.waitFor();
+	    System.out.println("RC=" + rc);
+	  }
+	}
